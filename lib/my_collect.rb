b33fd24a29fc3c3887e48e115a8c615db8279a42
Annabel Wilmerding <awilmerding@Annabel-Wilmerdings-MB-Pro.local>
2021-01-14 09:27:41 -0800

    commit tree 820a295d5621f596a0959a2cef63f7c336d87eec

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    binding.pry
+    new_array << yield(array[i])
+    binding.pry
+    i += 1
+  end
+  new_array
+end
--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -19,7 +19,7 @@ describe "my_collect" do
 
   it "returns a new collection of appropriately modified elements, in this case capitalized languages" do
     expect(my_collect(languages) do |language|
-      language.upcase
+       language.upcase
     end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
   end
 
